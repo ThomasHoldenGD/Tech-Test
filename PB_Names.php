79af957f58e16480df8304400d5e6385a9ca9c4e
ThomasHoldenGD <thomasholdengd@gmail.com>
2016-01-07 12:28:40 +0000

    Overthinking
    It wasn't something overly convoluted as i originially though i think.

--- PB_Names.php
+++ PB_Names.php
@@ -1,6 +1,9 @@
 <?php
 //Project Penny Blossoms
 
+
+$dataFile = fopen("PB_Data.txt", "w") or die("Unable to open file!");
+
 $array = $_POST;
 foreach($array as $i => $info){	
 	foreach($info as $key => $value){
@@ -8,18 +11,8 @@ foreach($array as $i => $info){
 		$s_name = array_key_exists('surname', $value) ? $value['surname'] : null;
 		
 		$data = $f_name . " " . $s_name . "\r\n";
-		
-		$input = file_put_contents('PB_Data.txt', $data, FILE_APPEND | LOCK_EX);
-		
+		fwrite($dataFile,$data);
 	}
 }
-
-if($input === false){
-	die('Could not input in to file.');
-}else{
-	echo "Information inputted successfully.";
-}
-
-
-
+fclose($dataFile);
 ?>
